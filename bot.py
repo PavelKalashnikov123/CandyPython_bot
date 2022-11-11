2a435c9289538f35d213f4b7be8931973f8d3f5f
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-11-11 16:37:42 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,9 @@
+#Здесь создается бот и хранится его токен
+
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+
+bot = Bot(token='5790752460:AAGt5Sa4A6QXZdnfBKiUgEdcFa8S-svXizk')
+
+dp = Dispatcher(bot)
